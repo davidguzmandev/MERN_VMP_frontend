346f0d8a780b361939a837980bbc5121ea07574a
davidruncode <daguz.91@gmail.com>
2023-03-19 01:44:26 -0400

    Footer authlayout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import {Outlet} from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const AuthLayout = () => {
   return (
@@ -6,7 +7,7 @@ const AuthLayout = () => {
         <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-12 p-5 items-center">
             <Outlet />
         </main>
-        
+        <Footer/>
     </>
 
     
