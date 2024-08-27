bfab9055003eab999df863b3afb47e90e06ddc61
davidruncode <daguz.91@gmail.com>
2024-08-26 23:33:25 -0400

    update

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -3,5 +3,5 @@ import axios from "axios";
 const clienteAxios = axios.create ({
     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
 })
-
+console.log(`Base URL: ${import.meta.env.VITE_BACKEND_URL}/api`);
 export default clienteAxios;
